eed29d06cf076ee22fe3055ba63905cdd3ae8002
sv0ne <serjvolk222@gmail.com>
2023-02-04 21:39:50 +0200

    Коробочка + youWant

---
+++ dist/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	var w = $(window).outerWidth();
+	var h = $(window).outerHeight();
+	var isMobile = ('ontouchstart' in window);
+	const $body = $('body');
+	const BREAKPOINT_md1 = 1343;
+	const BREAKPOINT_1045 = 1044.98;
+	const BREAKPOINT_md2 = 992.98;
+	const BREAKPOINT_872 = 871.98;
+	const BREAKPOINT_md3 = 767.98;
+	const BREAKPOINT_552 = 551.98;
+	const BREAKPOINT_md4 = 479.98;
+
+	
+});
